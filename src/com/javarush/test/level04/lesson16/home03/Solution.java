59603af25ad90cd7ef350137594ed58f4deb5c3f
Денис Чилик <dzianis.chylik@mobicon-media.com>
2016-07-19 12:58:12 +0300

    Комментарии

--- src/com/javarush/test/level04/lesson16/home03/Solution.java
+++ src/com/javarush/test/level04/lesson16/home03/Solution.java
@@ -10,13 +10,29 @@ import java.io.*;
 public class Solution
 {
     public static void main(String[] args)   throws Exception {
+        // Первое: idea подчеркивает summ правильнее будет sum
         int summ = 0;
 
+        // Второе: сдесь лучше использовать цикл do {} while (Переделй с другим циклом)
         for (boolean exit = false; !exit; ) {
+            // Третье: ты создаешь reader в цикле постоянно (этого делать не нужно)
+            // Правило все что можно вынести наружу цикла должно быть вынесео наружу
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String s = reader.readLine();
+            // Четвертое: Обрати внимание что произойдет в случае если ты введешь не корректное значение
+            // произойдет исключение NumberFormatException и выполнение программы завершится
+            // Всегда проверяй на корректность ввода используй try {} catch (NumberFormatException) {}
+            // Две следующие строчки можно записать в виде одной
+            // пример:
+            // float b = Float.parseFloat("10.0");
+            // sum = sum + b;
+            // превратиться в
+            // sum += Float.parseFloat("10.0");
+            // старайся использовать запись a += b вместо a = a + b;
             int a = Integer.parseInt(s);
             summ = summ + a;
+
+            // При использование цикла do while эта запись перейдет в условие цикла и ты избавишься от лишней переменной
             exit = a == -1;
         }
 
