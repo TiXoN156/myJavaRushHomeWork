39c097dc3c505757522ecae65cfb31860aee1b0c
tihon.bobrov@gmail.com <tihon.bobrov@gmail.com>
2016-08-01 09:14:14 +0300

    Текущий курс

--- src/com/javarush/test/level04/lesson16/home03/Solution.java
+++ src/com/javarush/test/level04/lesson16/home03/Solution.java
@@ -3,46 +3,34 @@ package com.javarush.test.level04.lesson16.home03;
 import java.io.*;
 
 /* Посчитать сумму чисел
-Вводить с клавиатуры числа и считать их сумму. Если пользователь ввел -1, вывести на экран сумму и
-завершить программу.  -1 должно учитываться в сумме.
+Вводить с клавиатуры числа и считать их сумму. Если пользователь ввел -1, вывести на экран сумму и завершить программу.  -1 должно учитываться в сумме.
 */
-
-// ОДИН ХРЕН НЕ ПРОШЛА ТЕСТИРОВАНИЕ ;)
-// Тест
 
 public class Solution
 {
-    public static void main(String[] args)   throws Exception {
-
+    public static void main(String[] args) throws Exception
+    {
         double sum = 0;
-        // Эта строчка тебе не понпдобиться
-        boolean exit = false;
+        double number = 0;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         do
         {
+            String s = reader.readLine();
             try
             {
-                // Эту строчку нужно вынести за try catch блон поскольку она не бросает исключение
-                String s = reader.readLine();
-
-                // Достаточно один раз преобразовать строку в число
-                // присвоить это значение переменной number типа double
-                // потомучто операция преобразования строки в число выполняется долго и нет смысла делать ее дважды
-                // Переменную в которую ты будешь записывать число нужно вынести за пределы блока try и присвоить ей значение 0
-                // чтобы в случае ошибки она осталась равной 0
-                // потомучто она тебе понадобиться при проверке условия цикла
-                sum += Double.parseDouble(s);
-                // Собственно эта строчка будет не нужна а  Double.parseDouble(s) != -1 перенесется в цикл
-                exit = Double.parseDouble(s) != -1;
+                number = Double.parseDouble(s);
+                sum += number;
             }
-            catch (NumberFormatException e){
+            catch (NumberFormatException e)
+            {
                 System.out.println("Неверный ввод, введите число.");
             }
         }
-        while (!exit); // Сюда нельзя вставить проверку равно ли -1 значение Double.parseDouble(s), поэтому оставил переменную exit
+        while (number != -1);
 
         System.out.println(sum);
     }
 }
+
